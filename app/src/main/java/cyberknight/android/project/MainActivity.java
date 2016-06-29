74a12b9d070f3c6bb47639c8a35000af1501789c
Parth Prajapati <parthprajapati7995@gmail>
2016-06-29 23:59:00 +0530

    Attempt to solve exception in main

--- app/src/main/java/cyberknight/android/project/MainActivity.java
+++ app/src/main/java/cyberknight/android/project/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -70,7 +71,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        try {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }catch(NullPointerException npe){
+            Log.d("MainActivity","support action bar failed");
+        }
     }
 
     @Override
