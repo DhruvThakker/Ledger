4a5a42cfeba91d4a3a460935c3906d0f6896d6d4
Parth Prajapati <parthprajapati7995@gmail>
2016-06-29 21:22:10 +0530

    Added Files

---
+++ app/src/main/java/cyberknight/android/project/MainActivity.java
@@ -0,0 +1,61 @@
+package cyberknight.android.project;
+
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout navDrawer;
+    ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        navDrawer = (DrawerLayout) findViewById(R.id.navDrawer);
+        drawerToggle = new ActionBarDrawerToggle(this,navDrawer,R.string.open,R.string.close){
+            public void onDrawerClosed(View view){
+                super.onDrawerClosed(view);
+                //getActionBar().setTitle("done");
+            }
+            public void onDrawerOpened(View view){
+                super.onDrawerOpened(view);
+                //getActionBar().setTitle("open");
+            }
+        };
+        navDrawer.setDrawerListener(drawerToggle);
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+        //getActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
